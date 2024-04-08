2ff59f58cc1597a78f16b49a5f131b5a5e4f203f
Nicholas Oyagh <you@example.com>
2024-04-08 16:02:20 -0700

    corrected

--- package/dist/utils/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (message) => {
-    console.log(new Date().toISOString() + ": " + message);
-};
-exports.logger = logger;
-//# sourceMappingURL=logger.js.map
