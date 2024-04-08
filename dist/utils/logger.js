ff97fc9f54171693d28485806feeb410192d8707
Nicholas Oyagh <you@example.com>
2024-04-08 15:57:28 -0700

    new changes

--- dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (message) => {
-    console.log(new Date().toISOString() + ": " + message);
-};
-exports.logger = logger;
