1a89a7942b4468568ed8d055d6e33140a73a5613
Nicholas Oyagh <you@example.com>
2024-04-12 16:28:44 -0700

    renamed

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 require("dotenv/config");
-// const PORT = process.env.PORT || 8080;
-app_1.default.listen(8080, () =>
-  console.log(`Server running on http://localhost:8080`)
-);
+const PORT = process.env.PORT || 8080;
+app_1.default.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
